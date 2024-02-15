8512d54b2732dd0bd020e9715da12628e4d9b313
jlxq0 <jlxq0@users.noreply.github.com>
2024-02-15 07:53:36 +0000

    deploy: 746994ee0687e31c624ab7031d565a90f9a24d6f

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     './*.md',
     './*.html',
   ],
+  safelist: [
+    'bg-red-500',
+    'text-3xl',
+    'lg:text-4xl',
+    'text-teal-400',
+    'pt-4',
+    'pb-4',
+  ],
   darkMode: 'class',
   theme: {
     theme: {
